e2210502c58bb7e91675c44377b7e0931acaa3d4
Vinayak Menon <vviswana@gmail.com>
2021-06-08 14:36:46 -0500

    Adding user creation, along with endpoint to get user info (which is secured via Octa/OAuth)

--- src/main/java/com/vviswana/mpo/WebSecurityConfiguration.kt
+++ src/main/java/com/vviswana/mpo/WebSecurityConfiguration.kt
@@ -4,20 +4,26 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.web.util.matcher.RequestMatcher
-import javax.servlet.http.HttpServletRequest
+import com.okta.spring.boot.oauth.Okta
 
 @Configuration
 @EnableWebSecurity
 open class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.requiresChannel()
-            .requestMatchers(RequestMatcher { r: HttpServletRequest ->
-                r.getHeader(
-                    "X-Forwarded-Proto"
-                ) != null
-            })
-            .requiresSecure()
+        http.authorizeRequests()
+            // allow anonymous access to certain paths
+            .antMatchers("/podcasts").permitAll()
+            .antMatchers("/podcastdetails").permitAll()
+            .antMatchers("/podcastupdate").permitAll()
+            .antMatchers("/register_user").permitAll()
+
+            // all other requests
+            .anyRequest().authenticated()
+            .and()
+            .oauth2ResourceServer().jwt(); // replace .jwt() with .opaqueToken() for Opaque Token case
+
+        // Send a 401 message to the browser (w/o this, you'll see a blank page)
+        Okta.configureResourceServer401ResponseBody(http);
     }
 }
